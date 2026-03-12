981e3f5a9ce3b706d95c46120a0930771c7728a3
merrooo <amirmerrooo@gmail.com>
2026-03-12 12:33:33 +0200

    c

---
+++ public/Sec_Acc.js
@@ -0,0 +1,19 @@
+const initialData = [
+    {
+        "service": "4",
+        "user": "mm",
+        "pass": "78787"
+    },
+    {
+        "service": "bnbn",
+        "user": "bnbnb",
+        "pass": "bbnbnbn"
+    },
+    {
+        "service": "456456",
+        "user": "456456",
+        "pass": "456456"
+    }
+];
+
+// Last updated: 3/12/2026, 12:15:43 PM
